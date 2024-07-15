094a39043823ac473c5873f285bead0502f79d1e
Adi Singh <adis@ntnu.no>
2024-07-14 21:58:34 -0400

    better interface with history

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
-# from pandasai.llm.local_llm import LocalLLM
-# import streamlit as st
-# import pandas as pd
-# from pandasai import SmartDataframe
-
-# model = LocalLLM(
-#      api_base= "http://localhost:11434",
-#      model = "llama3"
-#  )
-
-# st.title("Talk to your Llama friend")
-# uploaded_file = st.file_uploader("Upload your files for llama", type = ['csv'])
-
-# if uploaded_file is not None:
-#     data = pd.read_csv(uploaded_file)
-#     st.write(data.head(3))
-
-#     df = SmartDataframe(data, config={"llm": model})
-#     prompt = st.text_area("Enter your prompt:")
-
-# if st.button("Generate"):
-#         if prompt:
-#             with st.spinner("typing.."):
-#                 st.write(df.chat(prompt))
 
 
 from langchain_community.llms import Ollama
 import streamlit as st
 
+llm = Ollama(model="llama3")
 
-llm = Ollama(model = "llama3")
+st.title("Chat with Llama")
 
-st.title("Chat with llama")
-prompt = st.text_area("Start typing:")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-if st.button("Answer"):
-    if prompt: 
-        with st.spinner("Typing..."):
-            answer = st.write(llm.invoke(prompt, stop = ['< |eot_id| >']))
-#to have the answer print out in bit by bit style (stream) we can use st.write_stream and llm.stream instead
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
+# React to user input
+if prompt := st.chat_input("You:"):
+    # Display user message in chat message container
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = llm.invoke(prompt)
+            st.markdown(response)
+    
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
